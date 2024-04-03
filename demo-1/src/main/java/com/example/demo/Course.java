e509b77676f00c5acd9c084c8bcd8b43ede69819
영건 김 <young3702@naver.com>
2024-04-04 00:20:49 +0900

    20240404_javaStudy(courseClass)_added

--- demo-1/src/main/java/com/example/demo/Course.java
+++ demo-1/src/main/java/com/example/demo/Course.java
@@ -1,5 +1,15 @@
 package com.example.demo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="COURSE_DTLS")
 public class Course {
 
+	private Integer cid;
+	private String cname;
+	private Double price;
 }
