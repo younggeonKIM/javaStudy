a9dbc757635488c577d50df733663f4722129196
영건 김 <young3702@naver.com>
2024-04-05 03:36:37 +0900

    20240405_javaStudy(courseRepo, restController, reportService)_added

--- demo-1/src/main/java/com/example/demo/Course.java
+++ demo-1/src/main/java/com/example/demo/Course.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -9,6 +10,7 @@ import lombok.Data;
 @Table(name="COURSE_DTLS")
 public class Course {
 
+	@Id
 	private Integer cid;
 	private String cname;
 	private Double price;
---
+++ demo-1/src/main/java/com/example/demo/ReportRestController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+public class ReportRestController {
+
+	@Autowired
+	private ReportService reportService;
+	
+	@GetMapping("/excel")
+	public void generateExcelReport(HttpServletResponse response) throws Exception {
+		
+		response.setContentType("application/octet-stream");
+		
+		String headerKey="Content-Disposition";
+		String headerValue="attachment;filename=courses.xls";
+		
+		response.setHeader(headerKey, headerValue);
+		
+		reportService.generateExcel(response);
+	}
+}
